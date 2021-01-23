d57a25d412a76f99abc018ff5f6bdda5528d1649
Jonathan <jonbezeau@gmail.com>
2021-01-22 17:32:14 -0800

    Styled the success page

--- pages/success.js
+++ pages/success.js
@@ -5,7 +5,19 @@ export default function Success() {
     return (
         <>
             <Background />
-            <p>Survey Submitted Successfully</p>
+            <p className='surveyComplete'>Survey Submitted Successfully</p>
+            <style jsx>{`
+                .surveyComplete {
+                    position: fixed;
+                    top: 50%;
+                    transform: translateY(-50%);
+                    width: 100%;
+                    text-align: center;
+                    z-index: 2;
+                    font-size: 18px;
+                    color: white;
+                }
+            `}</style>
         </>
     )
 }
