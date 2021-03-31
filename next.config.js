27fb02c4fa85c72d6a1e2e03a06c7015009649ac
Jonathan Bezeau <UltravioletVoodoo@users.noreply.github.com>
2021-03-30 22:10:21 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     env: {
-        MONGO_URI: 'mongodb+srv://admin:TbhmO5qlCdozq7DH@cluster0.v9pia.mongodb.net/<dbname>?retryWrites=true&w=majority'
+        MONGO_URI: '<<INSERT SECRETS HERE>>'
     }
 }
